03fe1d4f159ff5d302ac41f91aab5932cb9752dc
thefrens <levin2fort@gmail.com>
2022-04-21 10:34:35 -0500

    Initaial commit

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import logo from '../assests/images/logo.svg';
+import '../assests/styles/App.scss';
+
+function Btn({ img, title, price }) {
+  return (
+    <div className="App">
+        <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default Btn;
